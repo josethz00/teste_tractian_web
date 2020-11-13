bb67e979361f2205fd6a24215824782a3a711881
josethz00 <jtsoares17@hotmail.com>
2020-11-12 23:22:50 -0300

    new components created

--- src/components/RegisterCompany/index.tsx
+++ src/components/RegisterCompany/index.tsx
@@ -1,22 +1,36 @@
 import { Button, Input } from 'antd';
-import React, { useEffect } from 'react';
+import React, { useRef } from 'react';
+import api from '../../services/api';
 import FormContainer from '../FormContainer';
 
 
 const RegisterCompany: React.FC = () => {
 
-  useEffect(() => {
+  const companyInputRef = useRef<Input>(null);
 
-    console.log('o');
+  async function handleSubmit () {
 
-  });
+    try {
+
+      const { data } = await api.post('companies/store', {
+        name: companyInputRef.current!.input.value
+      });
+      alert(data);
+
+    } catch (err) {
+
+      alert('Não foi possível cadastrar a empresa');
+
+    }
+
+  }
 
   return (
 
     <FormContainer>
       <h1>Companies registration form</h1>
-      <Input placeholder='Company name' className='input' size='large' style={{ width: 500, margin: '20px 0px' }} />
-      <Button size='large' style={{ width: 500 }}>
+      <Input placeholder='Company name' required ref={companyInputRef} className='input' size='large' style={{ width: 500, margin: '20px 0px' }} />
+      <Button size='large' style={{ width: 500 }} onClick={handleSubmit}>
         Register
       </Button>
     </FormContainer>
